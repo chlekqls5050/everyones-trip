392c1a078c42f91999e84858394d5c28e545ed78
dabeen <chlekqls5050@naver.com>
2025-01-23 03:38:07 +0900

    더보기 버튼 효과 수정

--- src/app/list/[id]/page.tsx
+++ src/app/list/[id]/page.tsx
@@ -4,6 +4,7 @@ import style from './page.module.css';
 import ListItem from '@/components/list-item';
 import { tripData } from "@/types";
 import { useEffect, useState, use } from 'react';
+import Image from "next/image";
 
 
 export default function Page({ params }: { params: Promise<{ id: string }>}) {
@@ -13,10 +14,10 @@ export default function Page({ params }: { params: Promise<{ id: string }>}) {
     const [count, setCount] = useState(12);
 
     const fetchData = async (pageNo:number) => {
-        const response = await fetch(`https://apis.data.go.kr/B551011/KorService1/areaBasedSyncList1?numOfRows=${count}&MobileOS=ETC&MobileApp=trip&serviceKey=${process.env.NEXT_PUBLIC_API_KEY}&_type=json&arrange=Q&contentTypeId=${id}`, {cache : "force-cache"}); // contentId=1&
+        const response = await fetch(`https://apis.data.go.kr/B551011/KorService1/areaBasedSyncList1?numOfRows=${count}&MobileOS=ETC&MobileApp=trip&serviceKey=${process.env.NEXT_PUBLIC_API_KEY}&_type=json&arrange=Q&contentTypeId=${id}`, {cache : "force-cache"});
         const data = await response.json();
         setData(data.response.body.items.item);
-        
+
           if(!response.ok) {
             if(response.status === 404) {
               NotFound();
@@ -31,6 +32,16 @@ export default function Page({ params }: { params: Promise<{ id: string }>}) {
 
     return (
         <div className={style.container}>
+            <div className={style.banner_wrap}>
+                <div className={style.banner_img_wrap}>
+                    <Image src={`/images/sub_banner_${id}.jpg`} fill alt={'배너 이미지지'} />
+                </div>
+                <div className={style.banner_txt_wrap}>
+                    <p className={style.title}>
+                        {id === "32" ? "STAY" : "FESTIVTIES"}
+                    </p>
+                </div>
+            </div>
             <div className='w-1200'>
                 <div className={style.list_wrap}>
                     { 
