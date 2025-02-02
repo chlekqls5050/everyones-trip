b7917e236e8de3e9ceede84aa4f8ed9a4f9d6d7d
dabeen <chlekqls5050@naver.com>
2025-02-03 00:41:26 +0900

    header 레이아웃 변경

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,25 +7,30 @@ export default function Header(){
     return (
         <header className={`${style.common_header}`}>
           <div className="w-1200 h-100">
-            <Link href={'/'} className={style.hd_logo_wrap}>
-                <div>
-                    {/* Everyone&apos;s trip */}
-                    <span>모두의 여행</span>
-                    <Image src={'/images/icon/icon_logo_map.png'} alt='로고 아이콘' width={30} height={30} />
+            <div className={style.header_wrap}>
+                <Link href={'/'} className={style.hd_logo_wrap}>
+                    <div>
+                        {/* Everyone&apos;s trip */}
+                        <span>모두의 여행</span>
+                        <Image src={'/images/icon/icon_logo_map.png'} alt='로고 아이콘' width={30} height={30} />
+                    </div>
+                </Link>
+                <div className={style.hd_right_wrap}>
+                    <div className={style.hd_menu_wrap}>
+                        <ul>
+                            <li>
+                                <Link href={'/list/12'}>Travel</Link>
+                            </li>
+                            <li>
+                                <Link href={'/board/list/notice'}>Notice</Link>
+                            </li>
+                            <li>
+                                <Link href={'/board/list/event'}>Event</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <HeaderUtillties />
                 </div>
-            </Link>
-            <div className={style.hd_menu_wrap}>
-                <ul>
-                    <li>
-                        <Link href={'/list/12'}>Travel Guide</Link>
-                    </li>
-                    <li>
-                        <Link href={'/board/list'}>Notice</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className={style.hd_right_wrap}>
-                <HeaderUtillties />
             </div>
           </div>
         </header>
