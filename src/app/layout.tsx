87a0652cdb4b3eba01dee0cc78c9d16210143d5c
dabeen <chlekqls5050@naver.com>
2025-02-03 16:35:27 +0900

    mete 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import "./globals.css";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title:"모두의 여행",
+  description:"여행 코스부터 명소까지, 모두의 여행이 함께합니다.",
+  openGraph: {
+    title:"모두의 여행",
+    description:"여행 코스부터 명소까지, 모두의 여행이 함께합니다.",
+    // images:['/thumbnail.png']
+  }
+};
 
 
 export default function RootLayout({
