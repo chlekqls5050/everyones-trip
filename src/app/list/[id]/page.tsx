73d772f4bea6e494ee0472a9a21291cf53d82aa6
dabeen <chlekqls5050@naver.com>
2025-02-03 16:06:11 +0900

    배너 이미지 추가

--- src/app/list/[id]/page.tsx
+++ src/app/list/[id]/page.tsx
@@ -13,6 +13,7 @@ export default function Page({ params }: { params: Promise<{ id: string }>}) {
     const { id } = use(params);
     const [count, setCount] = useState<number>(12);
     const [data, setData] = useState([]);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     const fetchData = useCallback(async (count:number) => {
         const response = await fetch(`https://apis.data.go.kr/B551011/KorService1/areaBasedSyncList1?numOfRows=${count}&MobileOS=ETC&MobileApp=trip&serviceKey=${process.env.NEXT_PUBLIC_API_KEY}&_type=json&arrange=Q&contentTypeId=${id}`, { cache: "force-cache" });
@@ -46,39 +47,47 @@ export default function Page({ params }: { params: Promise<{ id: string }>}) {
                 </div>
             </div>
             <div className={style.sub_nav_wrap}>
-                <div>
-                    <Link href={'/'}>홈</Link>
-                    <div>
-                        <p>
-                            {id === "12" ? "Spot" : id === "14" ? "cultural facilities" : id === "15" ? "festival" : id === "25" ? "Travel course" : id === "28" ? "leisure sports" : id === "32" ? "Stay" : id === "38" ? "shopping" : id === "39" ? "eating house" : "ALL"}
-                        </p>
-                        <ul>
-                            <li>
-                                <Link href={'/list/12'}>spot</Link>
-                            </li>
-                            <li>
-                                <Link href={'/list/14'}>cultural facilities</Link>
-                            </li>
-                            <li>
-                                <Link href={'/list/15'}>festival</Link>
-                            </li>
-                            <li>
-                                <Link href={'/list/25'}>Travel course</Link>
-                            </li>
-                            <li>
-                                <Link href={'/list/28'}>leisure sports</Link>
-                            </li>
-                            <li>
-                                <Link href={'/list/32'}>Stay</Link>
-                            </li>
-                            <li>
-                                <Link href={'/list/38'}>shopping</Link>
-                            </li>
-                            <li>
-                                <Link href={'/list/39'}>eating house</Link>
-                            </li>
-                        </ul>
-                    </div>
+                <Link href={'/'}>
+                    <Image src={'/images/icon/icon_home.png'} alt={'home 버튼'} width={20} height={20} />
+                </Link>
+                <div className={style.nav_tab_wrap}>
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className={isOpen ? style.open : ""}
+                >
+                        {id === "12" ? "Spot" : id === "14" ? "cultural facilities" : id === "15" ? "festival" : id === "25" ? "Travel course" : id === "28" ? "leisure sports" : id === "32" ? "Stay" : id === "38" ? "shopping" : id === "39" ? "eating house" : "ALL"}
+                        <span>
+                            <Image src={`/images/icon/icon_arrow.png`} width={12} height={12} alt={'arrow 아이콘'} />
+                        </span>
+                    </button>
+                    {isOpen && (
+                    <ul>
+                        <li>
+                            <Link href={'/list/12'}>spot</Link>
+                        </li>
+                        <li>
+                            <Link href={'/list/14'}>cultural facilities</Link>
+                        </li>
+                        <li>
+                            <Link href={'/list/15'}>festival</Link>
+                        </li>
+                        <li>
+                            <Link href={'/list/25'}>Travel course</Link>
+                        </li>
+                        <li>
+                            <Link href={'/list/28'}>leisure sports</Link>
+                        </li>
+                        <li>
+                            <Link href={'/list/32'}>Stay</Link>
+                        </li>
+                        <li>
+                            <Link href={'/list/38'}>shopping</Link>
+                        </li>
+                        <li>
+                            <Link href={'/list/39'}>eating house</Link>
+                        </li>
+                    </ul>
+                    )}
                 </div>
             </div>
             <div className='w-1200'>
