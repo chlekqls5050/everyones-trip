182779a912af8cbfbe63f2561f99c1e1e47546ca
dabeen <chlekqls5050@naver.com>
2025-02-10 14:08:19 +0900

    메인 게시판 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { supabase } from "@/lib/supabase";
 import FestivitiesSection from "@/components/festivities-section";
 import LodgmentSection from "@/components/lodgment-section";
+import { supabase } from "@/lib/supabase";
 import Visual from "@/components/visual";
 import style from "./page.module.css";
 import Link from "next/link";
@@ -9,7 +9,8 @@ import MainBoardItem from "@/components/main-board-item";
 
 async function getFestivitiesData() {
   const response = await fetch(
-    `https://apis.data.go.kr/B551011/KorService1/searchFestival1?numOfRows=4&MobileOS=ETC&MobileApp=festivites&_type=json&arrange=Q&eventStartDate=20250115&serviceKey=${process.env.NEXT_PUBLIC_API_KEY}`
+    `https://apis.data.go.kr/B551011/KorService1/searchFestival1?numOfRows=4&MobileOS=ETC&MobileApp=festivites&_type=json&arrange=Q&eventStartDate=20250115&serviceKey=${process.env.NEXT_PUBLIC_API_KEY}`,
+    { cache: "force-cache" }
   );
   const result = await response.json();
   return result.response.body.items.item;
@@ -17,38 +18,55 @@ async function getFestivitiesData() {
 
 async function getLodgmentData() {
   const response = await fetch(
-    `https://apis.data.go.kr/B551011/KorService1/searchStay1?numOfRows=7&MobileOS=ETC&MobileApp=lodgment&_type=json&arrange=R&serviceKey=${process.env.NEXT_PUBLIC_API_KEY}`
+    `https://apis.data.go.kr/B551011/KorService1/searchStay1?numOfRows=7&MobileOS=ETC&MobileApp=lodgment&_type=json&arrange=R&serviceKey=${process.env.NEXT_PUBLIC_API_KEY}`,
+    { cache: "force-cache" }
   );
   const result = await response.json();
   return result.response.body.items.item;
 }
-
-async function getBoardData() {
+export async function getBoardData() {
   const { data: eventPosts, error: eventError } = await supabase
-    .from("board")
-    .select("*")
-    .eq("type", "event")
+    .from('board')
+    .select('*')
+    .eq('type', 'event')
     .limit(5);
+
   const { data: noticePosts, error: noticeError } = await supabase
-    .from("board")
-    .select("*")
-    .eq("type", "notice")
+    .from('board')
+    .select('*')
+    .eq('type', 'notice')
     .limit(5);
 
   if (eventError || noticeError) {
-    console.error("데이터 가져오기 오류", eventError, noticeError);
+    console.error('데이터 가져오기 오류', eventError, noticeError);
     return { eventPosts: [], noticePosts: [] };
   }
 
   return { eventPosts, noticePosts };
 }
-
 export default async function Home() {
-  const [festivitiesItems, lodgmentItems, boardData] = await Promise.all([
-    getFestivitiesData(),
-    getLodgmentData(),
-    getBoardData(),
-  ]);
+  const festivitiesItems = await getFestivitiesData();
+  const lodgmentItems = await getLodgmentData();
+
+
+  const { eventPosts, noticePosts } = await getBoardData();
+
+
+  // const { data: eventPosts, error: eventError } = await supabase
+  //   .from("board")
+  //   .select("*")
+  //   .eq("type", "event")
+  //   .limit(5);
+  // const { data: noticePosts, error: noticeError } = await supabase
+  //   .from("board")
+  //   .select("*")
+  //   .eq("type", "notice")
+  //   .limit(5);
+
+  // if (eventError || noticeError) {
+  //   console.error("데이터 가져오기 오류", eventError, noticeError);
+  //   return <div>데이터 가져오는 데 문제가 발생했습니다.</div>;
+  // }
 
   return (
     <div className={style.container}>
@@ -65,14 +83,14 @@ export default async function Home() {
                   <h3>공지사항</h3>
                   <Link href={'/board/list/notice'}>View More <span className={style.plus_sign}>+</span></Link>
                 </div>
-                <MainBoardItem posts={boardData.noticePosts || []} />
+                <MainBoardItem posts={noticePosts || []} />
               </div>
               <div>
                 <div className={style.main_title_wrap}>
                   <h3>이벤트</h3>
                   <Link href={'/board/list/event'}>View More <span className={style.plus_sign}>+</span></Link>
                 </div>
-                <MainBoardItem posts={boardData.eventPosts || []} />
+                <MainBoardItem posts={eventPosts || []} />
               </div>
             </div>
           </div>
